c90eb25d22664f6e8d03ac7e07555c131ee78974
peddi <saikumarpeddi03@gmail.com>
2026-06-20 16:49:04 +0530

    first commot file

---
+++ sneivlp-vote-lock-service/src/test/java/com/foodapplication/sneivlpvotelockservice/SneivlpVoteLockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapplication.sneivlpvotelockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneivlpVoteLockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
