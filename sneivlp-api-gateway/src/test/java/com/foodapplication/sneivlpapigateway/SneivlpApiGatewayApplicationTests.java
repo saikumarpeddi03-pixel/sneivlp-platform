82a92a917e9deae6e80b94aca543e5239798886e
peddi <saikumarpeddi03@gmail.com>
2026-06-20 18:09:16 +0530

    api gateway
    port numbers are added through api gateway

---
+++ sneivlp-api-gateway/src/test/java/com/foodapplication/sneivlpapigateway/SneivlpApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapplication.sneivlpapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneivlpApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ sneivlp-audit-service/src/main/java/com/foodapplication/sneivlpauditservice/SneivlpAuditServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foodapplication.sneivlpauditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneivlpAuditServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneivlpAuditServiceApplication.class, args);
+    }
+
+}
