7c5c5f25345d5756e8daf660fb3529aa5ec8e0be
peddi <saikumarpeddi03@gmail.com>
2026-06-20 15:44:15 +0530

    project setup 1

---
+++ sneivlp-identity-service/src/main/java/com/foodapplication/sneivlpidentityservice/SneivlpIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foodapplication.sneivlpidentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneivlpIdentityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneivlpIdentityServiceApplication.class, args);
+    }
+
+}
